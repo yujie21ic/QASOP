5d06050a2186d2c52dc5aab9b474f589d072a40b
余天 <yutiansut@yutiandeMacBook-Pro.local>
2019-12-29 17:16:15 +0800

    #update

---
+++ QASOP/__init__.py
@@ -0,0 +1,18 @@
+#
+
+
+class workspace():
+    def __init__(self):
+        pass
+
+    def generate_backtest_example(self):
+        """
+        generate a backtest example for people to use
+        """
+        pass
+
+    def generate_simulate_example(self):
+        pass
+
+    def generate_realaccount_binding(self):
+        pass
